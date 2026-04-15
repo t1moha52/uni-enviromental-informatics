1104557c4da32ac46d71f5de016af9c32f3d0efb
T1imoha_Laptop <tmaxikov@gmail.com>
2026-04-15 12:09:42 +0200

    Buttons have been added.

--- Semester 2/Calculator WPF/Calculator/MainWindow.xaml.cs
+++ Semester 2/Calculator WPF/Calculator/MainWindow.xaml.cs
@@ -16,27 +16,6 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string text = "Nichego";
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void btn_HelloWorld_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hello World!");
-            text = "Hello World?";
-            
-        }
-
-        private void rbtn_FirstOption_Checked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("This is first option.");
-        }
-
-        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            this.TextBox1.Text = text;
-        }
+       
     }
 }
